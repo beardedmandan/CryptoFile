8fcc31dd5cd941e98cb1246ed39720a0cbb45861
Dan Harris <dan@beardedman.info>
2015-05-20 02:11:35 +0930

    Version 1

--- backup/CryptoUtils.java
+++ backup/CryptoUtils.java
@@ -14,7 +14,6 @@ import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.math.BigInteger;
-import java.nio.ByteBuffer;
 import java.util.Arrays;
 
 import java.security.InvalidKeyException;
@@ -34,9 +33,6 @@ import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
-import javax.crypto.ShortBufferException;
-import javax.crypto.spec.DHPrivateKeySpec;
-import javax.crypto.spec.DHPublicKeySpec;
 import javax.crypto.spec.SecretKeySpec;
 
 /**
@@ -50,7 +46,6 @@ import javax.crypto.spec.SecretKeySpec;
  * @author www.mykong.com (http://www.mkyong.com/java/java-sha-hashing-example/)
  * @author javamex.com
  * (http://javamex.com/tutorials/cryptography/rsa_encryption.shtml)
- * @author http://coding.westreicher.org/?p=23
  */
 public class CryptoUtils {
 
@@ -140,73 +135,55 @@ public class CryptoUtils {
     }
 
     
-    public static void doCryptoRSA(char mode, String algorithm, File inputFile, File outputFile) throws CryptoException, ShortBufferException {
+    public static void generateKey() throws NoSuchAlgorithmException, InvalidKeySpecException, IOException {
+        KeyPairGenerator kpg = KeyPairGenerator.getInstance("RSA");
+        kpg.initialize(2048);
+        KeyPair kp = kpg.genKeyPair();
+        Key publicKey = kp.getPublic();
+        Key privateKey = kp.getPrivate();
+
+        KeyFactory fact = KeyFactory.getInstance("RSA");
+        RSAPublicKeySpec pub = fact.getKeySpec(kp.getPublic(), RSAPublicKeySpec.class);
+        RSAPrivateKeySpec priv = fact.getKeySpec(kp.getPrivate(), RSAPrivateKeySpec.class);
+
+        saveToFile("public.key", pub.getModulus(), pub.getPublicExponent());
+        saveToFile("private.key", priv.getModulus(), priv.getPrivateExponent());
+        System.out.println("Successful Private/Public Key generation..");
+    }
+
+    
+    public static void doCryptoRSA(char mode, File inputFile, File outputFile) throws CryptoException {
         try {
-            //intialise variables
-            ALGORITHM = algorithm;
-            Cipher cipher;
-            byte[] inputBytes = new byte[(int) inputFile.length()];
-            byte[] output = new byte[0];
-            
-            //prepare input for encryption/decryption in byte format
-            FileInputStream inputStream = new FileInputStream(inputFile);
-            inputStream.read(inputBytes);
+            Cipher cipher = Cipher.getInstance("RSA");
             
             if(mode == 'd'){ //decrypt mode
-                //load previously generated key
-                PrivateKey privKey;
-                privKey = readRSAPrivKeyFromFile("private.key"); //RSA encryption
-                cipher = Cipher.getInstance(ALGORITHM);
+                PrivateKey privKey = readPrivKeyFromFile("private.key");
                 cipher.init(Cipher.DECRYPT_MODE, privKey);
-                
-                output = blockCipher(inputBytes, Cipher.DECRYPT_MODE, cipher);
-                System.out.println("Successful Decryption..");   
-                
+                System.out.println("Successful Decryption..");
             } else { //encrypt mode
-                //load previously generated key
-                PublicKey pubKey;   
-                pubKey = readRSAPubKeyFromFile("public.key"); //RSA encryption
-                cipher = Cipher.getInstance(ALGORITHM);
+                PublicKey pubKey = readPubKeyFromFile("public.key");                
                 cipher.init(Cipher.ENCRYPT_MODE, pubKey);
-                             
-                output = blockCipher(inputBytes, Cipher.ENCRYPT_MODE, cipher);
                 System.out.println("Successful Encryption..");
             }
             
-            //write output
+            FileInputStream inputStream = new FileInputStream(inputFile);
+            byte[] inputBytes = new byte[(int) inputFile.length()];
+            inputStream.read(inputBytes);
+            
+            byte[] outputBytes = cipher.doFinal(inputBytes);
+            
             FileOutputStream outputStream = new FileOutputStream(outputFile);
-            outputStream.write(output);
+            outputStream.write(outputBytes);
             
             inputStream.close();
             outputStream.close();
-            
         } catch (NoSuchPaddingException | NoSuchAlgorithmException | InvalidKeyException | BadPaddingException | IllegalBlockSizeException | IOException ex) {
             throw new CryptoException("Error encrypting/decrypting file", ex);
         }
     }
 
     
-    public static void generateKey(String algorithm) throws NoSuchAlgorithmException, InvalidKeySpecException, IOException {
-        ALGORITHM = algorithm;
-        
-        KeyPairGenerator kpg = KeyPairGenerator.getInstance(ALGORITHM);
-        kpg.initialize(1024); //1024 bit key length
-        KeyPair kp = kpg.genKeyPair();
-        Key publicKey = kp.getPublic();
-        Key privateKey = kp.getPrivate();
-
-        KeyFactory fact = KeyFactory.getInstance(ALGORITHM);
-        
-        RSAPublicKeySpec pub = fact.getKeySpec(kp.getPublic(), RSAPublicKeySpec.class);
-        RSAPrivateKeySpec priv = fact.getKeySpec(kp.getPrivate(), RSAPrivateKeySpec.class);
-        saveRSAKeyToFile("public.key", pub.getModulus(), pub.getPublicExponent());
-        saveRSAKeyToFile("private.key", priv.getModulus(), priv.getPrivateExponent());
-        
-        System.out.println("Successful Private/Public Key generation..");
-    }
-            
-    
-    private static void saveRSAKeyToFile(String fileName, BigInteger mod, BigInteger exp) throws IOException {
+    private static void saveToFile(String fileName, BigInteger mod, BigInteger exp) throws IOException {
         ObjectOutputStream oout = new ObjectOutputStream(new BufferedOutputStream(new FileOutputStream(fileName)));
         try {
             oout.writeObject(mod);
@@ -217,9 +194,9 @@ public class CryptoUtils {
             oout.close();
         }
     }
- 
+
     
-    private static PublicKey readRSAPubKeyFromFile(String keyFileName) throws CryptoException, IOException {
+    private static PublicKey readPubKeyFromFile(String keyFileName) throws CryptoException, IOException {
         InputStream in = new FileInputStream(keyFileName);
         ObjectInputStream oin = new ObjectInputStream(new BufferedInputStream(in));
         try {
@@ -237,7 +214,7 @@ public class CryptoUtils {
     }
     
     
-     private static PrivateKey readRSAPrivKeyFromFile(String keyFileName) throws CryptoException, IOException {
+     private static PrivateKey readPrivKeyFromFile(String keyFileName) throws CryptoException, IOException {
         InputStream in = new FileInputStream(keyFileName);
         ObjectInputStream oin = new ObjectInputStream(new BufferedInputStream(in));
         try {
@@ -252,48 +229,5 @@ public class CryptoUtils {
         } finally {
             oin.close();
         }
-    }
-     
-     
-    private static byte[] blockCipher(byte[] bytes, int cipherMode, Cipher cipher) throws IllegalBlockSizeException, BadPaddingException{
-	// initialize 2 buffers.	
-	byte[] scrambled = new byte[0];	// scrambled will hold intermediate results
-	byte[] toReturn = new byte[0]; // toReturn will hold the total result
-        
-	// if we encrypt we use 100 byte long blocks. Decryption requires 128 byte long blocks (because of RSA)
-        // key is set at 1024 for RSA
-	int length = (cipherMode == Cipher.ENCRYPT_MODE)? 100 : 128;
-
-	byte[] buffer = new byte[length];
-
-	for (int i=0; i< bytes.length; i++){           
-		// if we filled our buffer array we have our block ready for de- or encryption
-		if ((i > 0) && (i % length == 0)){              
-			scrambled = cipher.doFinal(buffer);
-			toReturn = append(toReturn,scrambled); // add the result to our total result.
-			int newlength = length; // calculate length of next buffer required
-
-			// if newlength is longer than remaining bytes in the bytes array it is shortened.
-			if (i + length > bytes.length) {
-				 newlength = bytes.length - i;
-			}		
-			buffer = new byte[newlength]; // clean buffer array
-		}		
-		buffer[i%length] = bytes[i]; // copy byte into our buffer.
-	}
-
-	// this step is needed if a trailing buffer is found when encrypting.
-	scrambled = cipher.doFinal(buffer);
-	toReturn = append(toReturn,scrambled);
-
-	return toReturn;
-    }
-    
-    
-    private static byte[] append(byte[] prefix, byte[] suffix){
-	byte[] toReturn = new byte[prefix.length + suffix.length];
-        System.arraycopy(prefix, 0, toReturn, 0, prefix.length);
-        System.arraycopy(suffix, 0, toReturn, prefix.length, suffix.length);
-	return toReturn;
     }
 }
